05c6b8119ee4d4f314c8b65fee424ace9a1ee6a4
kld1974 <kellyldoyle@yahoo.com>
2023-06-06 15:37:26 -0700

    updates

---
+++ scripts.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $('#ai_source').hover(function() {
+      $('#ai_source_detail').toggle("slide");
+    });
+});
+
+
+$(document).ready(function(){
+    $('#ai_prep').hover(function() {
+      $('#ai_prep_detail').toggle("slide");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $('#ai_analysis').hover(function() {
+      $('#ai_analysis_detail').toggle("slide");
+    });
+});
