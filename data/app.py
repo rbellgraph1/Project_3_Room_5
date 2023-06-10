432905d55687f0cf19afd051bb738a4f867d327d
rbellgraph1 <38970921+rbellgraph1@users.noreply.github.com>
2023-06-09 21:50:56 -0700

    updated the app.py and juniper LOL

--- data/app.py
+++ data/app.py
@@ -1,27 +1,20 @@
-# Import the dependencies.
 import numpy as np
-from sqlalchemy import create_engine, inspect, func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
-
 app = Flask(__name__)
+#################################################
 # Database Setup
-engine = create_engine("sqlite:///cleaned_tracker_data.db", echo=False)
-
-# Create a Base object
+#################################################
+engine = create_engine("sqlite:///Project_3_Room_5/data/tracker_info.sqlite")
+# reflect an existing database into a new model
 Base = automap_base()
-
-# Reflect the database tables
+# # reflect the tables
 Base.prepare(engine, reflect=True)
-
-# # Map the classes to the tables
-Class1 = Base.classes.table1_name
-Class2 = Base.classes.table2_name
-
-# Create a session
-session = Session(engine)
+# # Save reference to the table
+# print(engine.table_names())
 
 
 
@@ -29,22 +22,39 @@ session = Session(engine)
 def welcome():
     return (
         "Tracker Foundations<br>"
-        f"/api/v1.0/data<br>"
+        f"/api/v1.0/tracker_info<br/>"
     )
 
-@app.route('/api/v1.0/data', methods=['GET'])
-def api_data():
-    results = session.query(Class1, Class2).\
-              filter(Class1.column_name == desired_value).\
-              all()
 
-    # Return a dictionary with all columns.
-    result_dict = {}
-    for i, result in enumerate(results):
-        result_dict[i] = {'Class1_col1': result.Class1_col1, 'Class1_col2': result.Class1_col2,
-                          'Class2_col1': result.Class2_col1, 'Class2_col2': result.Class2_col2}
+@app.route("/api/v1.0/tracker_info")
+def index():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    #  Query all all
+    results = session.execute("select * from tracker").all()
+    # Convert list of tuples into normal list
+    all_tracker_info = []
+    for res in results:
+        tracker_dict = {}
+        tracker_dict["Company"] = res[1]
+        tracker_dict["Device"] = res[2]
+        tracker_dict["Croud funded"] = res[3]
+        tracker_dict["Country"] = res[4]
+        tracker_dict["Region"] = res[5]
+        tracker_dict["Release year"] = res[6]
+        tracker_dict["Type"] = res[7]
+        tracker_dict["Accelerometer"] = res[8]
+        tracker_dict["Gyroscope"] = res[9]
+        tracker_dict["Magnetometer"] = res[10]
+        tracker_dict["Barometer"] = res[11]
+        tracker_dict["GPS"] = res[12]
+        tracker_dict["Heart Rate"] = res[13]
+        all_tracker_info.append(tracker_dict)
+    print(len(all_tracker_info))
+    session.close()
+    return jsonify(all_tracker_info)
+    
 
-    return jsonify(result_dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
